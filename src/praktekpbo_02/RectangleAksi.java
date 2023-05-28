fd3de8ec45e0cd66c7d3e3003c3ee7c65dd9290f
rifsalmuhammad <rifsalmuhammad@users.noreply.github.com>
2023-05-28 22:05:10 +0800

    Mengisi Nilai Atribute

--- src/praktekpbo_02/RectangleAksi.java
+++ src/praktekpbo_02/RectangleAksi.java
@@ -9,5 +9,7 @@ package praktekpbo_02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar = 3;
     }
 }
